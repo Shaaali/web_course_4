ddb6dbcb421c621c9bb27d41a0c38a83096fb8c3
Shalitha Pathiranage <shali96shri@gmail.com>
2020-04-24 20:11:18 +0530

    passport jwt

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -5,7 +5,7 @@ const leaderRouter = express.Router();
 leaderRouter.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-
+const authenticate = require('../authenticate');
 const Leaders = require('../models/leaders');
 
 
@@ -19,7 +19,7 @@ leaderRouter.route('/')
     },(err)=> next(err))
     .catch((err)=>next(err));
 })
-.post((req,res,next)=> {
+.post(authenticate.verifyUser,(req,res,next)=> {
     Leaders.create(req.body)
     .then((leader) => {
         console.log('leader Created ', leader);
@@ -29,11 +29,11 @@ leaderRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req,res,next)=> {
+.put(authenticate.verifyUser,(req,res,next)=> {
     res.statusCode = 403;
     res.end('Put operation not supported on /leaders');
 })
-.delete((req,res,next)=> {
+.delete(authenticate.verifyUser,(req,res,next)=> {
     Leaders.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -53,11 +53,11 @@ leaderRouter.route('/:leaderId/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req,res,next)=> {
+.post(authenticate.verifyUser,(req,res,next)=> {
     res.statusCode = 403;
     res.end('Post operation not supported on /leaders/'+ req.params.leaderId);
 })
-.put((req,res,next)=> {
+.put(authenticate.verifyUser,(req,res,next)=> {
     Leaders.findByIdAndUpdate(req.params.leaderId, {
         $set: req.body
     }, { new: true })
@@ -68,7 +68,7 @@ leaderRouter.route('/:leaderId/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete((req,res,next)=> {
+.delete(authenticate.verifyUser,(req,res,next)=> {
     Leaders.findByIdAndRemove(req.params.leaderId)
     .then((resp) => {
         res.statusCode = 200;
